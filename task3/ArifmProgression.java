bed44a18bf5866048baffa655e8271908eafdeba
NikolaiBidonov <49115919+NikolaiBidonov@users.noreply.github.com>
2019-04-20 20:54:45 +0300

    task3

--- task3/ArifmProgression.java
+++ task3/ArifmProgression.java
@@ -1,10 +1,24 @@
 package ru.bidonov.task3;
 
+import java.util.Scanner;
+
 public class ArifmProgression {
     public static void main(String[] args) {
 
-        int D = 3;
-        for (int a = 1; a < 20; a = a + D) {
+        Scanner scanner = new Scanner (System.in);
+        System.out.print ("Введите начало прогрессии ");
+        while (!scanner.hasNextDouble ()) {
+            System.out.print("Вы ввели не число. Попробуйте снова ");
+            scanner.next ();
+        }
+        double first = scanner.nextDouble ();
+        System.out.print ("Введите шаг прогрессии ");
+        while (!scanner.hasNextDouble ()) {
+            System.out.println ("Вы ввели не число. Попробуйте снова ");
+            scanner.next ();
+        }
+        double D = scanner.nextDouble ();
+        for (double a = first; a < 20; a = a + D) {
             System.out.print(a + " ");
         }
     }
