e0d65b977133fdaadc54a969e36aa5c1fbd27513
NikolaiBidonov <49115919+NikolaiBidonov@users.noreply.github.com>
2019-05-18 13:43:15 +0300

    task 9

--- task9/Child.java
+++ task9/Child.java
@@ -1,44 +1,24 @@
 package ru.bidonov.task9;
 
-import java.util.Scanner;
-
 public class Child {
-    Scanner scanner = new Scanner (System.in);
-    private int choice;
-    private FoodList[] foodList = new FoodList[8];
 
+    private Food [] goodFood = {Food.APPLE, Food.CAKE, Food.SOUP};
 
-    public void eat () throws BadFoodException {
-        System.out.println ("Чем накормим ребенка?");
-        foodList[0] = new FoodList (Food.BROCCOLI);
-        foodList[1] = new FoodList (Food.APPLE);
-        foodList[2] = new FoodList (Food.CAKE);
-        foodList[3] = new FoodList (Food.CARROT);
-        foodList[4] = new FoodList (Food.FISH);
-        foodList[5] = new FoodList (Food.LEMON);
-        foodList[6] = new FoodList (Food.SOUP);
-        foodList[7] = new FoodList (Food.TOMATO);
-
-        for (int i = 0; i < foodList.length; i++) {
-            System.out.println ("Клавиша " + i + " - " + foodList[i].food.getTitle ());
-        }
-
-        choice = scanner.nextInt ();
-
-        for (int i = 0; i < foodList.length; i++) {
-            try {
-                if (choice == i) {
-                    System.out.println ("Съел " + foodList[i].food.getTitle ());
-                } else {
-                    throw new BadFoodException ();
+    public void eat (Food food) throws Exception {
+        try {
+            for (Food tmpFood : goodFood) {
+                if (food.equals (tmpFood)) {
+                    System.out.println ("Съел " + tmpFood.getTitle ());
+                    return;
                 }
-            } catch (BadFoodException e) {
-                System.out.println ("Внимание! Вы дали " + foodList[i].food.getTitle () + ": " + e.getMessage ());
-            } finally {
-                System.out.println ("Спасибо, мама!");
             }
+            throw new Exception ("Еда не съедена");
+        } finally {
+            System.out.println ("Спасибо, мама!");
         }
     }
 }
+
+
 
 
--- task9/Mother.java
+++ task9/Mother.java
@@ -3,11 +3,28 @@ package ru.bidonov.task9;
 public class Mother {
 
 
-    public static void main (String[] args) throws Exception {
+    public static void main (String[] args)  {
 
         Child child = new Child ();
 
-        child.eat ();
+        try {
+            child.eat (Food.APPLE);
+        } catch (Exception e) {
+            e.printStackTrace ();
+        }
+
+        try {
+            child.eat (Food.FISH);
+        } catch (Exception e) {
+            e.printStackTrace ();
+        }
+
+        try {
+            child.eat (Food.CAKE);
+        } catch (Exception e) {
+            e.printStackTrace ();
+        }
+
 
     }
 }
